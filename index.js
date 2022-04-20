f98ebeb99dd6f008cec5cbd40fb96ed9b88ade84
adriankiezik <kiezikadrian@gmail.com>
2022-04-20 20:50:20 +0200

    Fix cors issue

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("It's working!");
