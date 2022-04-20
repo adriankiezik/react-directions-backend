2052b680e859cf3dcf65f58a4a4d4710f615fa3d
adriankiezik <kiezikadrian@gmail.com>
2022-04-20 22:42:52 +0200

    Create route calculating price

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const findGeolocation = require("./routes/findGeolocation");
+const calculatePrice = require("./routes/calculatePrice");
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,11 +12,9 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
-app.use("/find", findGeolocation);
 
-app.get("/", (req, res) => {
-  res.send("It's working!");
-});
+app.use("/find", findGeolocation);
+app.use("/costs", calculatePrice);
 
 server.listen(PORT, () => {
   console.log("Server started on port: " + PORT);
---
+++ routes/calculatePrice.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const pricePerKm = req.query.priceperkm;
+  const distanceInKm = req.query.distance;
+
+  const amountOfDays = Math.floor(distanceInKm / 800);
+  console.log(amountOfDays);
+
+  // Convert to number with 2 decimal numbers.
+  const totalPrice = +(distanceInKm * pricePerKm + 1000 * amountOfDays).toFixed(
+    2
+  );
+
+  res.json({ totalPrice });
+});
+
+module.exports = router;
