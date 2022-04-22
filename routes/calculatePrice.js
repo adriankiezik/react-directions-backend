881095004c54ca9c370d232bcf266ae739600674
adriankiezik <kiezikadrian@gmail.com>
2022-04-22 16:03:22 +0200

    Fix calculate price

--- routes/calculatePrice.js
+++ routes/calculatePrice.js
@@ -2,15 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const pricePerKm = req.query.priceperkm;
-  const distanceInKm = req.query.distance;
+  let pricePerKm = req.query.priceperkm;
+  let distanceInKm = req.query.distance;
+
+  if (pricePerKm == null || distanceInKm == null) {
+    return;
+  }
+
+  pricePerKm = +decodeURI(pricePerKm);
+  distanceInKm = +decodeURI(distanceInKm);
 
   const amountOfDays = Math.floor(distanceInKm / 800);
 
   // Convert to number with 2 decimal numbers.
-  const totalPrice = +(distanceInKm * pricePerKm + 1000 * amountOfDays).toFixed(
-    2
-  );
+  const totalPrice =
+    +(distanceInKm * pricePerKm + 1000 * amountOfDays).toFixed(2) * 1.1;
 
   res.json({ totalPrice });
 });
